fe665c8a2cb50a81330fd43bdda9f2f217fae90a
Angel Sosa <gabrielo.09.angel@gmail.com>
2024-05-25 18:16:44 -0500

    refactor new service repository

--- src/main/java/com/pe/botica/dto/ServiceViewDTO.java
+++ src/main/java/com/pe/botica/dto/ServiceViewDTO.java
@@ -1,5 +1,6 @@
 package com.pe.botica.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ServiceViewDTO {
     private String name;
     private String description;
--- src/main/java/com/pe/botica/repository/ServiceRepository.java
+++ src/main/java/com/pe/botica/repository/ServiceRepository.java
@@ -1,6 +1,5 @@
 package com.pe.botica.repository;
 
-import com.pe.botica.dto.ProductViewDTO;
 import com.pe.botica.dto.ServiceViewDTO;
 import com.pe.botica.model.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,16 +13,17 @@ import java.util.UUID;
 public interface ServiceRepository extends JpaRepository<Service, UUID> {
     @Query(value = """
              SELECT new com.pe.botica.dto.ServiceViewDTO(
-             p.name,
-             p.description,
-             dp.price,
+             s.name,
+             s.description,
+             ds.price,
              u.name,
-             p.imageUrl,
+             s.imageUrl
             ) from Service s
               inner join DrugstoreService ds
               on s.id = ds.service.id
               inner join User u
               on ds.user.id = u.id
-            """)
+            """
+    )
     public List<ServiceViewDTO> getAllServices();
 }
